dfc82759849fc9e3ab11aef90e347b968f82f362
Ansh-Mishra <anshmishra684@gmail.com>
2026-04-12 23:54:38 +0530

    another big overhaul, adding in softwares section where first entry is about my games journal also did some cleanup of code and make things reusable than having multiple copies of code everywhere added a favicon too, and now the active pages are highlighted in navbar and footer.

--- navbar.js
+++ navbar.js
@@ -15,7 +15,75 @@ function loadNavbar(pathToNavbar = '/navbar.html') {
         const sidebar = document.querySelector('.sidebar');
         if (sidebar) sidebar.style.display = 'none';
       }
+
+      highlightNavbar();
+
     })
+    
     .catch(err => console.error('Navbar load error:', err));
 }
 
+// Highlight navbar
+function highlightNavbar() {
+  const navbar = document.getElementById('navbar-placeholder');
+  if (!navbar) return;
+
+  const links = navbar.querySelectorAll("a");
+  const currentPath = window.location.pathname;
+
+  links.forEach(link => {
+    const linkPath = link.getAttribute("href");
+
+    if (
+      linkPath === currentPath ||
+      (linkPath === "/index.html" && currentPath === "/")
+    ) {
+      link.classList.add("active");
+    }
+  });
+}
+
+
+
+// Inject favicon
+(function() {
+  if (!document.querySelector("link[rel*='icon']")) {
+    const link = document.createElement('link');
+    link.rel = 'icon';
+    link.type = 'image/png';
+    link.href = '/Resources/Images/pixelarts/notlazy256x256.png';
+    document.head.appendChild(link);
+  }
+})();
+
+// Insert footer
+document.addEventListener("DOMContentLoaded", function() {
+    const footerElement = document.getElementById('footer');
+
+    if (footerElement &&
+    !footerElement.classList.contains("no-auto-footer")) {
+        footerElement.innerHTML = `
+            <ul>
+                <li><a href="/index.html">Home</a></li>
+                <li><a href="/Pages/Games.html">Games</a></li>
+                <li><a href="/Pages/PixelArt.html">Pixel Art</a></li>
+                <li><a href="/Pages/Softwares.html">Softwares</a></li>
+                <li><a href="/Pages/WebProjects.html">Web Projects</a></li>
+                <li><a href="/Pages/AboutMe.html">About Me</a></li>
+            </ul>
+        `;
+    }
+    // Highlight current page
+    const links = footerElement.querySelectorAll("a");
+    const currentPath = window.location.pathname;
+
+    links.forEach(link => {
+        const linkPath = link.getAttribute("href");
+        if (
+        linkPath === currentPath ||
+        (linkPath === "/index.html" && currentPath === "/")
+        ) {
+          link.classList.add("active");
+        }
+    });
+});
